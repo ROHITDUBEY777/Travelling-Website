2fd4ec8adb45f1c414b62d2f62671839fd1911fc
ROHIT DUBEY <rohitdubey713010@gmail.com>
2025-09-24 19:11:13 +0530

    added scroll smoother to the website

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,33 @@ import Hero from "./Components/hero"
 import About from "./Components/About"
 import Contact from "./Components/Contact"
 import Footer from "./Components/Footer"
+import gsap from "gsap"
+import { ScrollSmoother } from "gsap/ScrollSmoother"
+import { ScrollTrigger } from "gsap/ScrollTrigger" 
+import { useEffect } from "react"
+
+
+
+gsap.registerPlugin(ScrollSmoother,ScrollTrigger);
 function App() {
- 
+
+
+   useEffect(()=>{
+    if(!ScrollSmoother.get()){
+      ScrollSmoother.create({
+        content:"#smooth-content",
+        smooth:2,
+        effects:true,
+      })
+    }
+   },[])
+   
+
 
   return (
     <>
+     <div id="smooth-content" >
+
      <div className="text-white bg-[var(--background)] min-h-screen ">
 
      <Navbar />
@@ -17,6 +39,7 @@ function App() {
     <Contact />
     <Footer />
 
+     </div>
      </div>
     </>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
 
 const Navbar = () => {
 
@@ -9,19 +10,26 @@ const Navbar = () => {
   const handlescroll = (id) =>{
       const element = document.getElementById(id);
         if(element){
-          element.scrollIntoView({behavior:"smooth"})
+          const smoother = ScrollSmoother.get();
+           if(smoother){
+             smoother.scrollTo(element,true);
+           }
+           else{
+
+             element.scrollIntoView({behavior:"smooth"})
+           }
         }
 
   }
   return (
-    <div className='poppins fixed w-full bg-transparent  backdrop-blur-3xl text-2xl text-white  py-5  px-4  accent-zinc-100 z-100 justify-center text-center  md:justify-between flex flex-row '>
+    <div className='poppins fixed   w-full bg-transparent  backdrop-blur-3xl text-2xl text-white  py-5  px-4  accent-zinc-100 z-50  top-0 left-0 right-0 justify-center text-center  md:justify-between flex flex-row '>
        <h1 className=' text-center'>TravelSafe</h1>
        <nav className='   '>
         <ul className='hidden  md:flex flex-row text-xl gap-4'>
-            <li onClick={() => handlescroll("Hero")} className='hover:text-amber-400'>Home</li>
-            <li onClick={() => handlescroll("Footer")} className='hover:text-amber-400'>Testimonials</li>
-            <li onClick={() => handlescroll("About")} className='hover:text-amber-400'>About</li>
-            <li onClick={() => handlescroll("Contact")}  className='hover:text-amber-400'>Contact</li>
+            <li onClick={() => handlescroll("Hero")} className='cursor-pointer hover:text-amber-400'>Home</li>
+            <li onClick={() => handlescroll("Footer")} className=' cursor-pointer hover:text-amber-400'>Testimonials</li>
+            <li onClick={() => handlescroll("About")} className= ' cursor-pointer hover:text-amber-400'>About</li>
+            <li onClick={() => handlescroll("Contact")}  className=' cursor-pointer hover:text-amber-400'>Contact</li>
         </ul>
        </nav>
        <button className='cursor-pointer text-white border md:px-4 md:py-1 text-xl hover:text-black hover:bg-white hover:scale-105 transition-transform duration-300 rounded-3xl bg-black '>Book Now </button>
